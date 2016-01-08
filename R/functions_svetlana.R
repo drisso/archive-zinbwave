e0fbbcde6727e63d66cf40a9f0017545ac4ec9d1
Svetlana Gribkova <svetlana.gribkova@mines-paristech.fr>
2016-01-08 17:16:59 +0100

    added description of functions

--- R/functions_svetlana.R
+++ R/functions_svetlana.R
@@ -9,15 +9,26 @@ linkinv <- linkobj$linkinv
 # TODO: to document a function, do as in the 'standard.R' file of Davide, so that the documentation will be automatically created by roxygen 
 
 
+#' Log-likelihood function of the zero-inflated negative binomial model for the estimation of "right parts" with 
+#' log(M)=X.M*alpha.M+U*V; logit(Pi)=X.pi*alpha.pi+U*W; M=E[Y] and Y data matrix
+#' @param parms a vector of parameters: j-th column of alpha.M, followed by j-th column of V, followed by j-th column of alpha.pi, followed by j-th column of W, followed by the log(1/phi)
+#' @param j is the number of column (gene) to be optimized over
+#' @param Y the data matrix (cells in rows, genes in columns)
+#' @param X.M is the design matrix for M 
+#' @param X.pi is the design matrix for Pi.
+#' @param U is the current value for the latent factor matrix
+#' @param offsetx the offset for the regression on M
+#' @param offsetw the offset for the regression on Pi
+#' @param linkobj the link function object for the regression on pi (typically the result of binomial())
 
-#loglikelihood
-#parms should be a vector of parameters for gene j
-#first all M-parameters, then all Pi-parameters, then theta
 
-ziNegBin <- function(parms,j,X,Z,Y,epsilon=0) {
+ziNegBin <- function(parms,j,X.M,X.pi,U,Y,epsilon=0,offsetx=0,offsetz=0) {
+    X=cbind(X.M,U)
+    Z=cbind(X.pi,U)
+    kx=ncol(X)
+    kz=ncol(Z)
     # TODO : I added 'epsilon=0' as parameter otherwise it was undefined. Check this is correct
     mu <- as.vector(exp(X %*% parms[1:kx] + offsetx))
-    # TODO (ERROR) : kx and kz undefined. Either an argument of the function, or define it in the function
     phi <- as.vector(linkinv(Z %*% parms[(kx + 1):(kx + kz)] + 
                                  offsetz))
     theta <- exp(parms[(kx + kz) + 1])
@@ -31,8 +42,24 @@ ziNegBin <- function(parms,j,X,Z,Y,epsilon=0) {
     loglik-sum(parms[(kx + 1):(kx + kz)]^2)*epsilon
 }
 
+#' Gradient with respect to "right parts" of log-likelihood function of the zero-inflated negative binomial model with 
+#' log(M)=X.M*alpha.M+U*V; logit(Pi)=X.pi*alpha.pi+U*W; M=E[Y] and Y data matrix
+#' @param parms a vector of parameters: j-th column of alpha.M, followed by j-th column of V, followed by j-th column of alpha.pi, followed by j-th column of W, followed by the log(1/phi)
+#' @param j is the number of column (gene) to be optimized over
+#' @param Y the data matrix (cells in rows, genes in columns)
+#' @param X.M is the design matrix for M 
+#' @param X.pi is the design matrix for Pi.
+#' @param U is the current value for the latent factor matrix
+#' @param offsetx the offset for the regression on M
+#' @param offsetw the offset for the regression on Pi
+#' @param linkobj the link function object for the regression on pi (typically the result of binomial())
+
 #gradient function
-gradNegBin <- function(parms,j,X,Z,Y,epsilon=0) {
+gradNegBin <- function(parms,j,X.M,X.pi,U,Y,epsilon=0,offsetx=0,offsetz=0) {
+    X=cbind(X.M,U)
+    Z=cbind(X.pi,U)
+    kx=ncol(X)
+    kz=ncol(Z)
     eta <- as.vector(X %*% parms[1:kx] + offsetx)
     mu <- exp(eta)
     etaz <- as.vector(Z %*% parms[(kx + 1):(kx + kz)] + offsetz)
@@ -53,42 +80,43 @@ gradNegBin <- function(parms,j,X,Z,Y,epsilon=0) {
                                      (Y[,j] + theta)/(mu + theta), exp(-log(dens0) + log(1 - 
                                                                                              muz) + clogdens0) * (log(theta) - log(mu + theta) + 
                                                                                                                       1 - theta/(mu + theta)))
-    
-    # TODO: I think the use of epsilon below is wrong, (epsilon!=0) is binary, why not use epsilon directly?
-    colSums(cbind(wres_count * X, wres_zero * Z-2*parms[(kx + 1):(kx + kz)]*(epsilon!=0), wres_theta))
+    colSums(cbind(wres_count * X, wres_zero * Z-2*parms[(kx + 1):(kx + kz)]*epsilon, wres_theta))
 }
 
 
 
 ######################################################################################
 #Optimization with respect to U
+######################################################################################
+
+#' Log-likelihood function of the zero-inflated negative binomial model for the estimation of U in the model 
+#' log(M)=X.M*alpha.M+U*V; logit(Pi)=X.pi*alpha.pi+U*W; M=E[Y] with Y data matrix
+#' @param parms a vector of parameters: i-th line of U
+#' @param i is the number of ligne (cell) to be optimized over
+#' @param Y the data matrix (cells in rows, genes in columns)
+#' @param X.M is the design matrix for M 
+#' @param X.pi is the design matrix for Pi.
+#' @param V,W,alpha.M,alpha.pi are the "right parts" to be given as arguments
+#' @theta vector of length J with gene specific dispersion parameters
+#' @param linkobj the link function object for the regression on pi (typically the result of binomial())
+
 #log(M)=X.M%*%alpa.M+U%*%V, transpose the equation and consider X.M%*%alpha.M as known
 
-
-# parms here are elements of i-th column of t(U) (i-th line of U) 
-# all the other arguments are known
-# i is the number of line of U to be optimized over
-# V,W are from previous iteration
-# offset.M = t(alpha.M)%*%t(X.M)
-# offset.pi = t(alpha.pi)%*%t(X.pi)
-# counts is the matrix of counts
-# thetas is vector of thetas of all genes 
-
-#loglik which will take U as parameter
-
-ziNegBin.U <- function(parms,i,V,W,offset.M,offset.pi,counts,thetas){
-    thetas=exp(thetas)
-    is0counts <- counts == 0
-    n0counts <- counts != 0
+ziNegBin.U <- function(parms,i,V,W,alpha.M,alpha.pi,Y,theta){
+    offset.M=t(alpha.M)%*%t(X.M)
+    offset.pi=t(alpha.pi)%*%t(X.pi)
+    theta=exp(theta)
+    Y0 <- Y == 0
+    Y1 <- Y != 0
     mu <- as.vector(exp(t(V) %*% parms + offset.M[,i]))
     phi <- as.vector(linkinv(t(W) %*% parms + 
                                  offset.pi[,i]))
     loglik0 <- log(phi + exp(log(1 - phi) + suppressWarnings(dnbinom(0, 
-                                                                     size = thetas, mu = mu, log = TRUE))))
+                                                                     size = theta, mu = mu, log = TRUE))))
     # counts[i,] is i-th column of t(counts)
     loglik1 <- log(1 - phi) + suppressWarnings(dnbinom(counts[i,], 
-                                                       size = thetas, mu = mu, log = TRUE))
-    loglik <- sum(loglik0[is0counts[i,]]) + sum(loglik1[n0counts[i,]])
+                                                       size = theta, mu = mu, log = TRUE))
+    loglik <- sum(loglik0[Y0[i,]]) + sum(loglik1[Y1[i,]])
     loglik
 }
 #sum(sapply(seq(1000),function(i){ziNegBin.U(U[i,],i,V,W,t(alpha.M)%*%t(X.M),t(alpha.pi)%*%t(X.pi),counts,thetas)}))
@@ -96,21 +124,34 @@ ziNegBin.U <- function(parms,i,V,W,offset.M,offset.pi,counts,thetas){
 #sum(sapply(seq(20),function(j){ziNegBin(trueparam[,j],j)}))
 # gradient function for optimization in U
 
-gradNegBin.U <- function(parms,i,V,W,offset.M,offset.pi,counts,thetas) {
-    is0counts <- counts == 0
-    n0counts <- counts != 0
+#' Gradient of the log-likelihood function of the zero-inflated negative binomial model for the estimation of U in the model 
+#' log(M)=X.M*alpha.M+U*V; logit(Pi)=X.pi*alpha.pi+U*W; M=E[Y] with Y data matrix
+#' @param parms a vector of parameters: i-th line of U
+#' @param i is the number of ligne (cell) to be optimized over
+#' @param Y the data matrix (cells in rows, genes in columns)
+#' @param X.M is the design matrix for M 
+#' @param X.pi is the design matrix for Pi.
+#' @param V,W,alpha.M,alpha.pi are the "right parts" to be given as arguments
+#' @theta vector of length J with gene specific dispersion parameters
+#' @param linkobj the link function object for the regression on pi (typically the result of binomial())
+
+gradNegBin.U <- function(parms,i,V,W,alpha.M,alpha.pi,Y,theta) {
+    Y0 <- Y == 0
+    Y1 <- Y != 0
+    offset.M=t(alpha.M)%*%t(X.M)
+    offset.pi=t(alpha.pi)%*%t(X.pi)
     eta <- as.vector(t(V) %*% parms + offset.M[,i])
     mu <- exp(eta)
     etaz <- as.vector(t(W) %*% parms + offset.pi[,i])
     muz <- linkinv(etaz)
-    thetas <- exp(thetas)
-    clogdens0 <- dnbinom(0, size = thetas, mu = mu, log = TRUE)
-    dens0 <- muz * (1 - as.numeric(n0counts[i,])) + exp(log(1 - muz) + 
+    theta <- exp(theta)
+    clogdens0 <- dnbinom(0, size = theta, mu = mu, log = TRUE)
+    dens0 <- muz * (1 - as.numeric(Y1[i,])) + exp(log(1 - muz) + 
                                                             clogdens0)
-    wres_count <- ifelse(n0counts[i,],  counts[i,] - mu * (counts[i,] + thetas)/(mu + thetas), 
-                         -exp(-log(dens0) + log(1 - muz) + clogdens0 + log(thetas) - 
-                                  log(mu + thetas) + log(mu)))
-    wres_zero <- ifelse(n0counts[i,], -1/(1 - muz) * linkobj$mu.eta(etaz), 
+    wres_count <- ifelse(Y1[i,],  counts[i,] - mu * (counts[i,] + theta)/(mu + thetas), 
+                         -exp(-log(dens0) + log(1 - muz) + clogdens0 + log(theta) - 
+                                  log(mu + theta) + log(mu)))
+    wres_zero <- ifelse(Y1[i,], -1/(1 - muz) * linkobj$mu.eta(etaz), 
                         (linkobj$mu.eta(etaz) - exp(clogdens0) * linkobj$mu.eta(etaz))/dens0)
     colSums(wres_count * t(V) + wres_zero * t(W))
 }
