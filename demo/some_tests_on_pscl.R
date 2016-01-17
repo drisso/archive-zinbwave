95f768d50adf006989fcf61dad75534ed01eb968
Svetlana Gribkova <svetlana.gribkova@mines-paristech.fr>
2016-01-17 10:07:44 +0100

    numerical tests

--- demo/some_tests_on_pscl.R
+++ demo/some_tests_on_pscl.R
@@ -1,15 +1,19 @@
-# goal: simulate data 100 times with the same value of parameters (we take one gene only) 
-# and check the variability of estimators
 library(pscl)
+source('~/GitHub/zinb/R/functions_svetlana.R')
+
+# goal: simulate data K times with the same value of parameters (we take one gene only) 
+# and check the variability of estimators
+
 # sample size
-n=100
+n=50
+K=500
 
 # matrix to store the results of simulations
-results=matrix(0,nrow=100,ncol=5)
+results=matrix(0,nrow=K,ncol=5)
+X1=2*runif(n)
+U1=3*(1+runif(n))
+for (k in 1:K){
 
-for (k in 1:100){
-    X1=2*runif(n)
-    U1=3*(1+runif(n))
     X=cbind(X1,U1)
     
     alphaM=c(1.5,0.8)
@@ -19,7 +23,7 @@ for (k in 1:100){
     Pi=binomial()$linkinv(X%*%alphaPi)
     gene.exp=NULL
     for(i in 1:n){
-        gene.exp[i]=rnbinom(1,mu=M[i],size=log(4))
+        gene.exp[i]=rnbinom(1,mu=M[i],size=1)
     }
     gene.is1=NULL
     for(i in 1:n){
@@ -32,9 +36,9 @@ for (k in 1:100){
     colnames(tab)=c("expr","X1","X2")
     testdata=data.frame(tab)
     
-    tab2=cbind(gene.exp,X)
-    colnames(tab2)=c("expr","X1","X2")
-    testdata2=data.frame(tab2)
+#     tab2=cbind(gene.exp,X)
+#     colnames(tab2)=c("expr","X1","X2")
+#     testdata2=data.frame(tab2)
     
     estimate=zeroinfl(expr~X1+X2-1|X1+X2-1,data=testdata,dist="negbin",link="logit")
     results[k,1:4]=c(estimate$coefficients$count,estimate$coefficients$zero)
@@ -43,11 +47,15 @@ for (k in 1:100){
 }
 
 #plot histograms for estimators of each parameter
-hist(results[,1])
-hist(results[,2])
-hist(results[,3])
-hist(results[,4])
-hist(results[,5])
+hist(results[,1],breaks=50,xlab="alpha_M (true = 1.5)",main="sample size n=150, 500 simulations")
+hist(results[,2],breaks=50)
+hist(results[,3],breaks=50,xlab="alpha_Pi (true = 0.3)",main="sample size n=150, 500 simulations")
+hist(results[,4],breaks=50,xlab="alpha_Pi (true = 0.05)",main="sample size n=150, 500 simulations")
+hist(results[,5],breaks=50,xlab="theta (true = 1.38)",main="sample size n=150, 500 simulations")
+
+
+#quantiles
+quantile(results[,3],probs=c(0.05,0.25,0.5,0.75,0.95))
 
 #now test that my functions give the same result on these data
 # matrix with one gene only
@@ -55,7 +63,7 @@ Y=matrix(gene.exp.zeroinf,ncol=1)
 j=1
 #same predictors for M and for Pi
 Z=X
-library(zinb)
-optim(fn=ziNegBin,gr=gradNegBin,j=j,X.M=matrix(X1,ncol=1),X.pi=X.M,U=matrix(U1,ncol=1),Y=Y,par=c(1,1,0.5,0.1,1),control=list(fnscale=-1),method="BFGS")
 
-ziNegBin
+optim(fn=ziNegBin,gr=gradNegBin,j=j,X.M=matrix(X1,ncol=1),X.pi=matrix(X1,ncol=1),U=matrix(U1,ncol=1),Y=Y,par=c(1,1,0.5,0.1,1),control=list(fnscale=-1),method="BFGS")
+
+
