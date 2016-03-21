8381299f03fdebf6adbc2db8e15978af9bc39875
Svetlana Gribkova <svetlana.gribkova@mines-paristech.fr>
2016-03-21 18:21:33 +0100

    modif

--- R/functions_svetlana.R
+++ R/functions_svetlana.R
@@ -167,7 +167,9 @@ zinb = function(datamatrix){
     n=nrow(datamatrix)
     J=ncol(datamatrix)
     PCA.init=prcomp(log(1+datamatrix),center=TRUE,scale.=TRUE)
+    #U.0=matrix(1,nrow=n,ncol=2)
     U.0=PCA.init$x[,1:2]
+    #V.0=matrix(1,nrow=2,ncol=J)
     V.0=t(PCA.init$rotation[,1:2])
     colnames(U.0)=NULL
     colnames(V.0)=NULL
@@ -176,34 +178,35 @@ zinb = function(datamatrix){
     alt.number=25 #max number of alternations
     total.lik=rep(0,alt.number)
  
-    #OPTIMIZATION PROCEDURE
+    #UNE VERSION ALTERNATIVE AVEC PENALIZATION
     for (alt in 1:alt.number){
         print(alt)
         #evaluate total likelihood before alternation num alt
-        total.lik[alt]=sum(sapply(1:n,function(t) ziNegBin.U(U.0[t,],t,V.0,W.0,X.M=F,X.pi=F,alpha.M=F,alpha.pi=F,theta0,Y=datamatrix)))
+        total.lik[alt]=sum(sapply(1:n,function(t) ziNegBin.U(U.0[t,],t,V.0,W.0,X.M=F,X.pi=F,alpha.M=F,alpha.pi=F,theta0,Y=gene.exp.zeroinf)))
+        #if the increase in likelihood is smaller than 0.5%, stop maximization
+        #if(alt>1){if(abs((total.lik[alt]-total.lik[alt-1])/total.lik[alt-1])<0.005)break}
         print(total.lik[alt])
         #if the increase in likelihood is smaller than 0.5%, stop maximization
         # if(alt>1){if(abs((total.lik[alt]-total.lik[alt-1])/total.lik[alt-1])<0.005)break}
         if(alt>1){if(abs(total.lik[alt]-total.lik[alt-1])<1)break}
         #optimization for V and W, by gene, theta is optimized also
-        for (gene in 1:J){        
-            estimate=optim(fn=ziNegBin,gr=gradNegBin,j=gene,U=U.0,par=c(V.0[,gene],W.0[,gene],log(theta0)[gene]),Y=datamatrix,
+        for (gene in 1:J){            
+            estimate=optim(fn=ziNegBin,gr=gradNegBin,j=gene,U=U.0,par=c(V.0[,gene],W.0[,gene],log(theta0)[gene]),Y=gene.exp.zeroinf,
                            control=list(fnscale=-1),method="BFGS",epsilon=0.001)$par 
             V.0[,gene]=estimate[1:length(V.0[,gene])]
-            W.0[,gene]=estimate[(length(V.0[,gene])+1):(length(V.0[,gene])+length(W.0[,gene]))]        
-            theta0[gene]=exp(estimate[length(V.0[,gene])+length(W.0[,gene])+1])
+            W.0[,gene]=estimate[(length(V.0[,gene])+1):(length(V.0[,gene])+length(W.0[,gene]))]
+            theta0[gene]=min(exp(estimate[length(V.0[,gene])+length(W.0[,gene])+1]),10)       
         }
         #optimization for U, by cell, V,W and theta are fixed 
-        print("Step2")
         for(cell in 1:n){
-            U.0[cell,]=optim(fn=ziNegBin.U,gr=gradNegBin.U,i=cell,par=U.0[cell,],Y=datamatrix,
+            U.0[cell,]=optim(fn=ziNegBin.U,gr=gradNegBin.U,i=cell,par=U.0[cell,],Y=gene.exp.zeroinf,
                              theta=log(theta0),V=V.0,W=W.0,X.M=F,X.pi=F,
                              alpha.M=F,alpha.pi=F,
                              control=list(fnscale=-1),method="BFGS")$par        
         }
         alt=alt+1
-    }
-    zinb.result <- list(U=U.0,V=V.0,theta=theta0)
+    }    
+    zinb.result <- list(U=U.0,V=V.0,W=W.0,theta=theta0)
 }
 
 
