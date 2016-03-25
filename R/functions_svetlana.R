90e51056292264f836175eac1b58a90e9daae2c9
jpvert <djipay@gmail.com>
2016-03-25 03:11:37 -0700

    add faster version zinb2

--- R/functions_svetlana.R
+++ R/functions_svetlana.R
@@ -222,3 +222,176 @@ zinb = function(datamatrix, alt.number=25){
 }
 
 
+zinbloglikelihood <- function(datamatrix, U, V, W, theta) {
+    # Compute the loglikelihood for the simple model with only U, V and W. 
+    # Choose the fastest direction to sum (rows or columns) depending on the size of the matrix
+    # WE DONT USE IT BECAUSE IT IS FASTER TO JUST COMPUTE IT DIRECTLY WITHOUT CALLING THIS FUNCTION
+    if (nrow(datamatrix) <= ncol(datamatrix)) {
+        return( sum(sapply(1:nrow(datamatrix),function(t) ziNegBin.U(U[t,],t,V,W,X.M=F,X.pi=F,alpha.M=F,alpha.pi=F,log(theta),Y=datamatrix))) )
+    } else {
+        return( sum(sapply(1:ncol(datamatrix),function(t) ziNegBin(c(V[,t],W[,t],log(theta)[t]), j=t, U=U,Y=datamatrix))) )
+    }
+}
+#' Estimation of latent factors from a count matrix
+#' @param datamatrix the count data matrix (cells in rows, genes in columns)
+#' @param k number of latent factors (default 2)
+#' @param alt.number maximum number of iterations (default 25)
+#' @param epsilon regularization parameter (default 0.1)
+#' @param verbose print information (default TRUE)
+#' @export
+zinb2 = function(datamatrix, k=2, alt.number=25, epsilon=0.1, verbose=TRUE, use.old=FALSE){
+    n=nrow(datamatrix)
+    J=ncol(datamatrix)
+    # Initialize U and V by PCA on log(count+1) matrix
+    PCA.init=prcomp(log(1+datamatrix),center=TRUE,scale.=TRUE)
+    U.0=PCA.init$x[,1:k]
+    V.0=t(PCA.init$rotation[,1:k])
+    colnames(U.0)=NULL
+    colnames(V.0)=NULL
+    # Initialize W and theta to 1
+    W.0=matrix(1,nrow=k,ncol=J)
+    theta0=rep(1,ncol(datamatrix))
+    
+    # for(l in 1:J){
+    #theta0[l]=fitdistr(datamatrix[,l],densfun="negative binomial")$estimate[1]
+    #}
+    
+    
+    total.lik=rep(NA,alt.number)
+    
+    #UNE VERSION ALTERNATIVE AVEC PENALIZATION
+    for (alt in 1:alt.number){
+        if (verbose) {cat("Iteration ",alt,"\n",sep="")}
+
+        #evaluate total likelihood before alternation num alt
+        total.lik[alt]=sum(sapply(1:n,function(t) ziNegBin.U(U.0[t,],t,V.0,W.0,X.M=F,X.pi=F,alpha.M=F,alpha.pi=F,log(theta0),Y=datamatrix)))
+        
+        
+        #if the increase in likelihood is smaller than 0.5%, stop maximization
+        #if(alt>1){if(abs((total.lik[alt]-total.lik[alt-1])/total.lik[alt-1])<0.005)break}
+        if (verbose) {cat("log-likelihood = ",total.lik[alt],"\n",sep="")}
+        #if the increase in likelihood is smaller than 0.5%, stop maximization
+        # if(alt>1){if(abs((total.lik[alt]-total.lik[alt-1])/total.lik[alt-1])<0.005)break}
+        if(alt>1){if(abs(total.lik[alt]-total.lik[alt-1])<1)break}
+        #optimization for V and W, by gene, theta is optimized also
+        
+        if (use.old) {
+            # OLD VERSION (to remove once the new version is validated)
+        ptm <- proc.time()
+        for (gene in 1:J){     
+            #test=zeroinfl(X1~0+X2+X3,data.frame(cbind(datamatrix[,gene],U.0)),start=list(count=V.0[,gene],zero=W.0[,gene],theta=theta0[gene]),dist="negbin")
+            #test=zeroinfl(X1~0+X2+X3,data.frame(cbind(datamatrix[,gene],U.0)),dist="negbin")
+            #print(ziNegBin(parms=c(V.0[,gene],W.0[,gene],log(theta0)[gene]),gene,X.M=NULL,X.pi=NULL,U=U.0,Y=datamatrix,epsilon=0,offsetx=0,offsetz=0))
+            #print(ziNegBin(parms=estimate,gene,X.M=NULL,X.pi=NULL,U=U.0,Y=datamatrix,epsilon=0,offsetx=0,offsetz=0))
+            estimate=optim(fn=ziNegBin,gr=gradNegBin,j=gene,U=U.0,par=c(V.0[,gene],W.0[,gene],log(theta0)[gene]),Y=datamatrix,control=list(fnscale=-1),method="BFGS",epsilon=0)$par 
+            V.0[,gene]=estimate[1:length(V.0[,gene])]
+            W.0[,gene]=estimate[(length(V.0[,gene])+1):(length(V.0[,gene])+length(W.0[,gene]))]
+            theta0[gene]=exp(estimate[length(V.0[,gene])+length(W.0[,gene])+1])    
+        }
+        ptm <- proc.time()-ptm
+        
+        } else {
+            # NEW version, faster and more precise
+            ptm <- proc.time()
+            estimate <- sapply(seq(J), function(gene) {optim(fn=ziNegBinNEW,gr=gradNegBinNEW,par=c(V.0[,gene],W.0[,gene],log(theta0)[gene]),U=U.0,Y=datamatrix[,gene],control=list(fnscale=-1),method="BFGS",epsilon=epsilon)$par })
+            if (verbose) {print(proc.time()-ptm)}
+            V.0 <- estimate[1:k,]
+            W.0 <- estimate[(k+1):(2*k),]
+            theta0 <- exp(estimate[(2*k+1),])
+        }
+       
+        
+        if (verbose) {cat("log-likelihood = ",sum(sapply(1:n,function(t) ziNegBin.U(U.0[t,],t,V.0,W.0,X.M=F,X.pi=F,alpha.M=F,alpha.pi=F,log(theta0),Y=datamatrix))),"\n",sep="")}
+        
+        #optimization for U, by cell, V,W and theta are fixed 
+        for(cell in 1:n){
+            U.0[cell,]=optim(fn=ziNegBin.U,gr=gradNegBin.U,i=cell,par=U.0[cell,],Y=datamatrix,
+                             theta=log(theta0),V=V.0,W=W.0,X.M=F,X.pi=F,
+                             alpha.M=F,alpha.pi=F,
+                             control=list(fnscale=-1),method="BFGS")$par        
+        }
+    }    
+    zinb.result <- list(U=U.0,V=V.0,W=W.0,theta=theta0)
+}
+
+
+
+
+#' Log-likelihood function of the zero-inflated negative binomial model for the estimation of "right parts" with 
+#' log(M)=X.M*alpha.M+U*V; logit(Pi)=X.pi*alpha.pi+U*W; M=E[Y] and Y data matrix
+#' @param parms a vector of parameters: j-th column of alpha.M, followed by j-th column of V, followed by j-th column of alpha.pi, followed by j-th column of W, followed by the log(1/phi)
+#' @param j is the number of column (gene) to be optimized over
+#' @param Y the data matrix (cells in rows, genes in columns)
+#' @param X.M is the design matrix for M 
+#' @param X.pi is the design matrix for Pi.
+#' @param U is the current value for the latent factor matrix
+#' @param offsetx the offset for the regression on M
+#' @param offsetw the offset for the regression on Pi
+#' @export
+ziNegBinNEW <- function(parms,X.M=NULL,X.pi=NULL,U,Y,epsilon=0,offsetx=0,offsetz=0) {
+        X=cbind(X.M,U)
+        Z=cbind(X.pi,U)
+        #    X=cbind(X.M,U)
+#    Z=cbind(X.pi,U)
+    kx=ncol(X)
+    kz=ncol(Z)
+    
+    mu <- as.vector(exp(X %*% parms[1:kx] + offsetx))
+    #    phi <- as.vector(linkinv(Z %*% parms[(kx + 1):(kx + kz)] + 
+    #                                 offsetz))
+    eta <- as.vector(Z %*% parms[(kx + 1):(kx + kz)] + offsetz)
+    
+    lognorm <- -eta - copula::log1pexp(-eta)
+    
+    theta <- exp(parms[(kx + kz) + 1])
+    
+    logPnb <- suppressWarnings(dnbinom(Y, size = theta, mu = mu, log = TRUE))
+    
+    sum(logPnb[Y>0]) + sum(logPnb[Y==0] + copula::log1pexp(eta[Y==0]-logPnb[Y==0])) + sum(lognorm) -sum(parms[(kx + 1):(kx + kz)]^2)*epsilon
+    
+}
+
+
+
+#' Gradient with respect to "right parts" of log-likelihood function of the zero-inflated negative binomial model with 
+#' log(M)=X.M*alpha.M+U*V; logit(Pi)=X.pi*alpha.pi+U*W; M=E[Y] and Y data matrix
+#' @param parms a vector of parameters: j-th column of alpha.M, followed by j-th column of V, followed by j-th column of alpha.pi, followed by j-th column of W, followed by the log(1/phi)
+#' @param j is the number of column (gene) to be optimized over
+#' @param Y the data matrix (cells in rows, genes in columns)
+#' @param X.M is the design matrix for M 
+#' @param X.pi is the design matrix for Pi.
+#' @param U is the current value for the latent factor matrix
+#' @param offsetx the offset for the regression on M
+#' @param offsetw the offset for the regression on Pi
+#' @export
+gradNegBinNEW <- function(parms,X.M=NULL,X.pi=NULL,U,Y,epsilon=0,offsetx=0,offsetz=0) {
+        X=cbind(X.M,U)
+        Z=cbind(X.pi,U)
+        #    X=cbind(X.M,U)
+#    Z=cbind(X.pi,U)
+    kx=ncol(X)
+    kz=ncol(Z)
+    eta <- as.vector(X %*% parms[1:kx] + offsetx)
+    mu <- exp(eta)
+    etaz <- as.vector(Z %*% parms[(kx + 1):(kx + kz)] + offsetz)
+    muz <- linkinv(etaz)
+    logtheta <- parms[(kx + kz) + 1]
+    theta <- exp(logtheta)
+    Y0 <- Y <= 0 #==1 if counts is 0
+    Y1 <- Y > 0 #==1 if count is not 0
+    clogdens0 <- dnbinom(0, size = theta, mu = mu, log = TRUE)
+    dens0 <- muz * (1 - as.numeric(Y1)) + exp(log(1 - muz) + 
+                                                      clogdens0)
+    wres_count <- ifelse(Y1, Y - mu * (Y + theta)/(mu + theta), 
+                         -exp(-log(dens0) + log(1 - muz) + clogdens0 + logtheta - 
+                                  log(mu + theta) + log(mu)))
+    wres_zero <- ifelse(Y1, -1/(1 - muz) * linkobj$mu.eta(etaz), 
+                        (linkobj$mu.eta(etaz) - exp(clogdens0) * linkobj$mu.eta(etaz))/dens0)
+    wres_theta <- theta * ifelse(Y1, digamma(Y + theta) - 
+                                     digamma(theta) + logtheta - log(mu + theta) + 1 - 
+                                     (Y + theta)/(mu + theta), exp(-log(dens0) + log(1 - 
+                                    muz) + clogdens0) * (logtheta - log(mu + theta) +
+                                    1 - theta/(mu + theta)))
+    c(colSums(wres_count * X), colSums(wres_zero * Z)-2*epsilon*parms[(kx + 1):(kx + kz)],sum(wres_theta))
+}
+
