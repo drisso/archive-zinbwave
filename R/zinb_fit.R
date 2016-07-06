2fe7ab65bf35ebc90eec94475721823a4c63c4d9
Davide Risso <risso.davide@gmail.com>
2016-07-05 17:22:36 -0700

    Check singularity of X and V; better defaults for zinbFit

--- R/zinb_fit.R
+++ R/zinb_fit.R
@@ -18,35 +18,56 @@
 setMethod("zinbFit", "matrix",
           function(Y, X=matrix(1, ncol=1, nrow=NCOL(Y)), 
                    V=matrix(1, ncol=1, nrow=NROW(Y)), 
-                   which_X_mu=1L, which_V_mu=1L,
-                   which_X_pi=1L, which_V_pi=1L, ...) {
+                   which_X_mu=seq_len(NCOL(X)), which_V_mu=seq_len(NCOL(V)),
+                   which_X_pi=seq_len(NCOL(X)), which_V_pi=seq_len(NCOL(V)),
+                   penalty_beta_mu, penalty_beta_pi,
+                   penalty_gamma_mu, penalty_gamma_pi, ...) {
 
     J = NROW(Y) # number of genes
     n = NCOL(Y) # number of samples
+
+    ## Check singularity of X and V
+    X_mu <- X[, which_X_mu, drop=FALSE]
+    X_pi <- X[, which_X_pi, drop=FALSE]
+    V_mu <- V[, which_V_mu, drop=FALSE]
+    V_pi <- V[, which_V_pi, drop=FALSE]
+
+    if(qr(X_mu)$rank < NCOL(X_mu)) {
+       stop("The matrix X_mu is singular!")
+    }
+    if(qr(X_pi)$rank < NCOL(X_pi)) {
+        stop("The matrix X_pi is singular!")
+    }
+    if(qr(V_mu)$rank < NCOL(V_mu)) {
+        stop("The matrix V_mu is singular!")
+    }
+    if(qr(V_pi)$rank < NCOL(V_pi)) {
+        stop("The matrix V_pi is singular!")
+    }
 
     ## Identify intercept and put its penalty to 0
     if(missing(penalty_beta_mu)) {
         val <- max(1, length(which_X_mu)*NROW(X))
         penalty_beta_mu <- rep(1/val, length(which_X_mu))
-        penalty_beta_mu[which(colSums(X[, which_X_mu, drop=FALSE]==rep(1, n))==n)] <- 0
+        penalty_beta_mu[which(colSums(X_mu==rep(1, n))==n)] <- 0
     }
 
     if(missing(penalty_beta_pi)) {
         val <- max(1, length(which_X_pi)*NROW(X))
         penalty_beta_pi <- rep(1/val, length(which_X_pi))
-        penalty_beta_pi[which(colSums(X[, which_X_pi, drop=FALSE]==rep(1, n))==n)] <- 0
+        penalty_beta_pi[which(colSums(X_pi==rep(1, n))==n)] <- 0
     }
     
     if(missing(penalty_gamma_mu)) {
         val <- max(1, length(which_V_mu)*NROW(V))
         penalty_gamma_mu <- rep(1/val, length(which_V_mu))
-        penalty_gamma_mu[which(colSums(V[, which_V_mu, drop=FALSE]==rep(1, J))==J)] <- 0
+        penalty_gamma_mu[which(colSums(V_mu==rep(1, J))==J)] <- 0
     }
 
     if(missing(penalty_gamma_pi)) {
         val <- max(1, length(which_V_pi)*NROW(V))
         penalty_gamma_pi <- rep(1/val, length(which_V_pi))
-        penalty_gamma_pi[which(colSums(V[, which_V_pi, drop=FALSE]==rep(1, J))==J)] <- 0
+        penalty_gamma_pi[which(colSums(V_pi==rep(1, J))==J)] <- 0
     }
 
     # Create a ZinbModel object
